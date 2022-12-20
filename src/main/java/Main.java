2fff7c478f4b2904d7cf9e969973ca56b440e7b9
haotn <haotn@vng.com.vn>
2022-12-20 12:46:25 +0700

    demo cicd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,12 @@
  * @date 18/12/2022 13:01
  */
 public class Main {
+	// ở đây, ta có haàm tính tổng như sau
 	public static int calc(int a, int b) {
 		return a + b;
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Calculate result: 5 + 3 = " + calc(5, 3));
+		System.out.println("Calculate result: 8 + 2 = " + calc(2, 8));
 	}
 }
